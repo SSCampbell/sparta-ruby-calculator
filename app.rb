be24bae50ce60e55d4a7e96e52394f06f24046db
unknown <Scampbell@spartaglobal.com>
2018-07-17 23:04:48 +0100

    CORECTIONS: Edited the user text output | Fixed Indentation | Corrected name of variable

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ def multi_sum (num_1, num_2)
   return num_1 * num_2
 end
 
-def div_sum (num_1, num_2)
+def divide_sum (num_1, num_2)
   return num_1 / num_2
 end
 
@@ -27,81 +27,83 @@ end
 def sqrt_sum (num_1)
   return Math.sqrt(num_1)
 end
+
 if calc_choice == "1"
-
-  $stdout.puts 'Which operation would you like to perform?'
-  $stdout.flush
-  operation_choice = $stdin.gets.chomp
-  if operation_choice == "SQRT"
-    $stdout.puts 'What is the number you want to Square Root?'
+    $stdout.puts 'Which operation would you like to perform SQRT OR POW?'
     $stdout.flush
-    num_1 = $stdin.gets.chomp.to_i
-    sum = sqrt_sum(num_1)
-    puts "#{num_1} the square root is #{sum}"
+    operation_choice = $stdin.gets.chomp
+      if operation_choice == "SQRT"
+        $stdout.puts 'What is the number you want to Square Root?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        sum = sqrt_sum(num_1)
+        puts "#{num_1} the square root is #{sum}"
 
-  elsif operation_choice == "POW"
-      $stdout.puts 'What is the first number?'
-      $stdout.flush
-      num_1 = $stdin.gets.chomp.to_i
-      $stdout.puts 'What is the second number?'
-      $stdout.flush
-      num_2 = $stdin.gets.chomp.to_i
+      elsif operation_choice == "POW"
+        $stdout.puts 'What is the first number?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        $stdout.puts 'What is the second number?'
+        $stdout.flush
+        num_2 = $stdin.gets.chomp.to_i
 
-      sum = pow_sum(num_1, num_2)
-      puts "#{num_1} ^ #{num_2}  =  #{sum}"
-  end
+        sum = pow_sum(num_1, num_2)
+        puts "#{num_1} ^ #{num_2}  =  #{sum}"
+      else
+        puts "Please Enter The Correct Values: "
+    end
 
 elsif calc_choice == "2"
 
-  $stdout.puts 'Which operation would you like to perform? 1-ADD 2-SUB 3-MULTI 4-DIV'
-  $stdout.flush
-  operation_choice = $stdin.gets.chomp
-  puts "You have chosen #{operation_choice}"
-  if operation_choice == "1"
-    $stdout.puts 'What is the first number?'
+    $stdout.puts 'Which operation would you like to perform? 1-ADD 2-SUB 3-MULTI 4-DIV'
     $stdout.flush
-    num_1 = $stdin.gets.chomp.to_i
-    $stdout.puts 'What is the second number?'
-    $stdout.flush
-    num_2 = $stdin.gets.chomp.to_i
+    operation_choice = $stdin.gets.chomp
+    puts "You have chosen #{operation_choice}"
+      if operation_choice == "1"
+        $stdout.puts 'What is the first number?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        $stdout.puts 'What is the second number?'
+        $stdout.flush
+        num_2 = $stdin.gets.chomp.to_i
 
-    sum = add_sum(num_1, num_2)
-    puts "#{num_1} + #{num_2}  =  #{sum}"
-
-
-  elsif operation_choice == "2"
-  $stdout.puts 'What is the first number?'
-  $stdout.flush
-  num_1 = $stdin.gets.chomp.to_i
-  $stdout.puts 'What is the second number?'
-  $stdout.flush
-  num_2 = $stdin.gets.chomp.to_i
-    sum = sub_sum(num_1, num_2)
-    puts "#{num_1} - #{num_2}  =  #{sum}"
+        sum = add_sum(num_1, num_2)
+        puts "#{num_1} + #{num_2}  =  #{sum}"
 
 
-  elsif operation_choice == "3"
-  $stdout.puts 'What is the first number?'
-  $stdout.flush
-  num_1 = $stdin.gets.chomp.to_i
-  $stdout.puts 'What is the second number?'
-  $stdout.flush
-  num_2 = $stdin.gets.chomp.to_i
-    sum = multi_sum(num_1, num_2)
-      puts "#{num_1} * #{num_2}  =  #{sum}"
+      elsif operation_choice == "2"
+        $stdout.puts 'What is the first number?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        $stdout.puts 'What is the second number?'
+        $stdout.flush
+        num_2 = $stdin.gets.chomp.to_i
+        sum = sub_sum(num_1, num_2)
+        puts "#{num_1} - #{num_2}  =  #{sum}"
 
 
-  elsif operation_choice == "4"
-  $stdout.puts 'What is the first number?'
-  $stdout.flush
-  num_1 = $stdin.gets.chomp.to_i
-  $stdout.puts 'What is the second number?'
-  $stdout.flush
-  num_2 = $stdin.gets.chomp.to_i
-    sum = div_sum(num_1, num_2)
-    puts "#{num_1} / #{num_2}  =  #{sum}"
+      elsif operation_choice == "3"
+        $stdout.puts 'What is the first number?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        $stdout.puts 'What is the second number?'
+        $stdout.flush
+        num_2 = $stdin.gets.chomp.to_i
+        sum = multi_sum(num_1, num_2)
+        puts "#{num_1} * #{num_2}  =  #{sum}"
 
-  else
-    puts "Please Enter An Operation: "
-  end
+
+      elsif operation_choice == "4"
+        $stdout.puts 'What is the first number?'
+        $stdout.flush
+        num_1 = $stdin.gets.chomp.to_i
+        $stdout.puts 'What is the second number?'
+        $stdout.flush
+        num_2 = $stdin.gets.chomp.to_i
+        sum = divide_sum(num_1, num_2)
+        puts "#{num_1} / #{num_2}  =  #{sum}"
+
+      else
+      puts "Please Enter An Operation: "
+      end
 end
